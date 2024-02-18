10705f37e98dcc15bb835e3a74b8d4e14a8e7499
Matt Collinge <mattycov@googlemail.com>
2024-02-18 23:21:26 +0000

    added dev flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "plasma-control";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # python packages
+            (python311.withPackages (ps: with ps; [
+              python-lsp-server
+              black
+              flake8
+            ]))
+          ];
+        };
+      });
+    };
+
+}
